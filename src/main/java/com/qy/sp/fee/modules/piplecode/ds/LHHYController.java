355499b33252cf1cc6305608d73158f780e9f624
shitr <tangrong.shi@kuailework.com>
2017-08-17 15:07:27 +0800

    修改东硕联合会员同步接收方式

--- src/main/java/com/qy/sp/fee/modules/piplecode/ds/LHHYController.java
+++ src/main/java/com/qy/sp/fee/modules/piplecode/ds/LHHYController.java
@@ -2,10 +2,13 @@ package com.qy.sp.fee.modules.piplecode.ds;
 
 import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping(value = "/piple")
@@ -15,18 +18,19 @@ public class LHHYController {
 	private LHHYService lhhyService;
 	@RequestMapping(value = "/dslhhy/sync" ,produces = {"application/json;charset=UTF-8"})
 	@ResponseBody
-	public String channelRequest(String code,String msg,String orderId,String mobile,String price,String chargeCode,String transmissionData){
+	public String channelRequest(@RequestBody String requestBody){
 		String resultMsg = "error";
 		try{
-			JSONObject bodyObject = new JSONObject();
-			bodyObject.put("code", code);
-			bodyObject.put("msg", msg);
-			bodyObject.put("orderId", orderId);
-			bodyObject.put("mobile", mobile);
-			bodyObject.put("price",price);
-			bodyObject.put("chargeCode",chargeCode);
-			bodyObject.put("transmissionData",transmissionData);
-			resultMsg = lhhyService.processPaySuccess(bodyObject);
+//			JSONObject bodyObject = new JSONObject();
+//			bodyObject.put("code", code);
+//			bodyObject.put("msg", msg);
+//			bodyObject.put("orderId", orderId);
+//			bodyObject.put("mobile", mobile);
+//			bodyObject.put("price",price);
+//			bodyObject.put("chargeCode",chargeCode);
+//			bodyObject.put("transmissionData",transmissionData);
+			JSONObject requestObject = JSONObject.fromObject(requestBody);
+			resultMsg = lhhyService.processPaySuccess(requestObject);
 		}
 		catch(Exception e){
 			e.printStackTrace();
