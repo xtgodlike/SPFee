045bb2aca00afcd36b014813ade5daa723f0304f
tian.xie <tian.xie@workbrother.com>
2018-01-03 10:58:16 +0800

    'PM1091'

---
+++ src/main/java/com/qy/sp/fee/modules/piplecode/ds/DSXDDBController.java
@@ -0,0 +1,38 @@
+package com.qy.sp.fee.modules.piplecode.ds;
+
+import net.sf.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping(value = "/piple")
+public class DSXDDBController {
+
+	@Resource
+	private DSXDDBService dsxddbService;
+	@RequestMapping(value = "/dsxddb/sync" ,produces = {"application/json;charset=UTF-8"})
+	@ResponseBody
+	public String channelRequest(@RequestBody String requestBody){
+		String resultMsg = "error";
+		try{
+//			JSONObject bodyObject = new JSONObject();
+//			bodyObject.put("code", code);
+//			bodyObject.put("msg", msg);
+//			bodyObject.put("orderId", orderId);
+//			bodyObject.put("mobile", mobile);
+//			bodyObject.put("price",price);
+//			bodyObject.put("chargeCode",chargeCode);
+//			bodyObject.put("transmissionData",transmissionData);
+			JSONObject requestObject = JSONObject.fromObject(requestBody);
+			resultMsg = dsxddbService.processPaySuccess(requestObject);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return resultMsg;
+	}
+}
